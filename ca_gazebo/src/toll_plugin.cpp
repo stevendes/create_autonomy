029f3bb44e48489c223f997dbea8fa0c32c0e5c5
stevendes <steven.desvars@gmail.com>
2019-11-14 12:55:42 -0300

    WIP PID in joint

--- ca_gazebo/src/toll_plugin.cpp
+++ ca_gazebo/src/toll_plugin.cpp
@@ -3,27 +3,76 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
+#include "gazebo/common/Time.hh"
+
+
+
+#include <ros/ros.h>
+#include "std_msgs/Float64.h"
+
 
 namespace gazebo
 {
   class ModelPush : public ModelPlugin
   {
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+/*    enum State
+    {
+	    UP,
+	    TIME_UP,
+	    DOWN,
+	    STOP_DOWN,
+    };*/
+
+    ros::NodeHandle nh_;
+    ros::Publisher dummy_publisher_=nh_.advertise<std_msgs::Float64>("doomsday", 1);
+    double curr_position;
+    double output;
+    double set_pos=1;
+
+
+//    bool time_flag= false;
+//    State g_state = UP;
+   
+
+    public:
+    ModelPush(): 
+      nh_("dummy_node")
+    {
+
+    }
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
     {
       // Store the pointer to the model
       this->model = _parent;
+
+      this->jointController=this->model->GetJointController();
+      this->joint = this->model->GetJoint("toll_joint");
+      this->pid = common::PID(100, 0, 0);
+      std::string name = model->GetJoint("toll_joint")->GetScopedName();
+      this->jointController->SetVelocityPID(name, this->pid);
+      this->jointController->SetVelocityTarget(name, 1.0);
 
       // Listen to the update event. This event is broadcast every
       // simulation iteration.
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(
           std::bind(&ModelPush::OnUpdate, this));
+          
     }
+
 
     // Called by the world update start event
     public: void OnUpdate()
     {
+//      double error;
       // Apply a small linear velocity to the model.
-      this->model->GetJoint("toll_joint")->SetVelocity(0, 0.1);
+//      this->curr_position = model->GetJoint("toll_joint")->Position();
+//      this->joint = model->GetJoint("toll_joint");
+
+      this->jointController->Update();
+     
+//      this->model->GetJoint("toll_joint")->SetVelocity(0, 0.1);
+
+      
     }
 
     // Pointer to the model
@@ -31,6 +80,14 @@ namespace gazebo
 
     // Pointer to the update event connection
     private: event::ConnectionPtr updateConnection;
+
+    /// brief Pointer to the joint.
+    private: physics::JointPtr joint;
+
+    /// brief A PID controller for the joint.
+    private: common::PID pid;
+
+    private: physics::JointControllerPtr jointController;   
   };
 
   // Register this plugin with the simulator
