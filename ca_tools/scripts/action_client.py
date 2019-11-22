6f75a2b8eb4cd212db0bb4bf7bb67ebb47294632
stevendes <steven.desvars@gmail.com>
2019-11-22 15:29:01 -0300

    Added passing the goal via launch-file

--- ca_tools/scripts/action_client.py
+++ ca_tools/scripts/action_client.py
@@ -2,8 +2,6 @@
 
 import rospy
 import actionlib
-import roslib
-roslib.load_manifest('ca_tools')
 from ca_tools.msg import FollowLineAction, FollowLineGoal
 from geometry_msgs.msg import Point
 
@@ -11,10 +9,10 @@ from geometry_msgs.msg import Point
 def robot_client():
     _client = actionlib.SimpleActionClient('action_server', FollowLineAction)
     _client.wait_for_server()
-    _goal_seconds = 300
+    _goal_seconds = rospy.get_param("/get_seconds")
     _goal_position = Point()
-    _goal_position.x = -1.7
-    _goal_position.y = 1.7
+    _goal_position.x = rospy.get_param("/get_pos_x")
+    _goal_position.y = rospy.get_param("/get_pos_y")
     goal = FollowLineGoal(goal_seconds=_goal_seconds, goal_position=_goal_position)
     _client.send_goal(goal)
     _client.wait_for_result()
