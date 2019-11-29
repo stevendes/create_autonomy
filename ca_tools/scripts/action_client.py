1097c1200806267218c133ebd8375ac2ab06f49e
stevendes <steven.desvars@gmail.com>
2019-11-29 18:23:02 -0300

    Adressed the last comments

--- ca_tools/scripts/action_client.py
+++ ca_tools/scripts/action_client.py
@@ -7,21 +7,21 @@ from geometry_msgs.msg import Point
 
 
 def robot_client():
-    _client = actionlib.SimpleActionClient('action_server', FollowLineAction)
-    _client.wait_for_server()
-    _goal_seconds = rospy.get_param("/get_seconds")
-    _goal_position = Point()
-    _goal_position.x = rospy.get_param("/get_pos_x")
-    _goal_position.y = rospy.get_param("/get_pos_y")
-    goal = FollowLineGoal(goal_seconds=_goal_seconds, goal_position=_goal_position)
-    _client.send_goal(goal)
-    _client.wait_for_result()
+    client = actionlib.SimpleActionClient('follow_line_action', FollowLineAction)
+    client.wait_for_server()
+    goal_secs = rospy.get_param("/get_seconds")
+    goal_pose = Point()
+    goal_pose.x = rospy.get_param("/get_pos_x")
+    goal_pose.y = rospy.get_param("/get_pos_y")
+    goal = FollowLineGoal(goal_seconds=goal_secs, goal_position=goal_pose)
+    client.send_goal(goal)
+    client.wait_for_result()
     return _client.get_result()
 
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('action_client')
+        rospy.init_node('follow_line_client')
         result = robot_client()
     except rospy.ROSInterruptException:
         pass
--- ca_tools/scripts/action_server.py
+++ ca_tools/scripts/action_server.py
@@ -10,24 +10,27 @@ from ca_tools.msg import FollowLineAction, FollowLineFeedback, FollowLineResult
 
 
 class RobotAction(object):
+    """
+    This class keeps track of the goals, check if the position goal is achieved, keep track of the total time and traveled distance since the 
+    start of the process
+    """
     _ERROR_TOLERANCE = 0.1
 
     def __init__(self, name):
-        self._curr_dist = 0.0
+        self._accumulated_dist = 0.0
         self._current_pos = Point()
         self._last_pos = Point()
         self._gts_first = True
-        self._feedback = FollowLineFeedback()
-        self._result = FollowLineResult()
+        self._current_pos.x = None
+        self._current_pos.y = None
         self._gts_subscriber = rospy.Subscriber("/create1/gts", Odometry, self.gts_callback)
         self._action_name = name
         self._as = actionlib.SimpleActionServer(
             self._action_name, FollowLineAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
         self._goal_reached = False
-        self._curr_time = rospy.Time.now()
-        self._r = rospy.Rate(2)
-
+#        self._curr_time = rospy.Time.now()
+        self._rate = rospy.Rate(2)
 
     def gts_callback(self, data):
         if self._gts_first:
@@ -39,24 +42,25 @@ class RobotAction(object):
             self._last_pos.y = self._current_pos.y
             self._current_pos.x = data.pose.pose.position.x
             self._current_pos.y = data.pose.pose.position.y
-            self._curr_dist += math.hypot(abs( self._last_pos.x - self._current_pos.x), self._last_pos.y -
+            self._accumulated_dist += math.hypot(abs( self._last_pos.x - self._current_pos.x), self._last_pos.y -
                                      self._current_pos.y)
 
-    def goal_reach_control(self,_goal_point):
-        _x_limit = (self._current_pos.x <= (_goal_point.x + self._ERROR_TOLERANCE)) and (self._current_pos.x
-        >= (_goal_point.x - self._ERROR_TOLERANCE))
-        _y_limit = (self._current_pos.y <= (_goal_point.y + self._ERROR_TOLERANCE)) and (self._current_pos.y
-        >= (_goal_point.y - self._ERROR_TOLERANCE))
-        return _x_limit and _y_limit
-
+    def goal_reach_control(self, goal_point):
+        x_limit = (self._current_pos.x <= (goal_point.x + self._ERROR_TOLERANCE)) and (self._current_pos.x
+        >= (goal_point.x - self._ERROR_TOLERANCE))
+        y_limit = (self._current_pos.y <= (goal_point.y + self._ERROR_TOLERANCE)) and (self._current_pos.y
+        >= (goal_point.y - self._ERROR_TOLERANCE))
+        return x_limit and y_limit
 
     def execute_cb(self, goal):
         # helper variables
         success = True
-        _goal_point = Point()
-        _goal_point = goal.goal_position
-        _goal_seconds = goal.goal_seconds
-        _init_time = rospy.Time.now()
+        goal_point = Point()
+        goal_point = goal.goal_position
+        goal_seconds = goal.goal_seconds
+        init_time = rospy.Time.now()
+        feedback = FollowLineFeedback()
+        result = FollowLineResult()
 
         while not self._goal_reached:
             if self._as.is_preempt_requested():
@@ -64,28 +68,28 @@ class RobotAction(object):
                 self._as.set_preempted()
                 success = False
                 break
-            self._curr_time = rospy.Time.now() - _init_time
-            self._feedback.accumulated_distance = self._curr_dist
-            self._feedback.current_time = self._curr_time
-            self._as.publish_feedback(self._feedback)
-            rospy.loginfo(self._feedback.accumulated_distance)
-            if self.goal_reach_control(_goal_point):
+            curr_time = rospy.Time.now() - init_time
+            feedback.accumulated_distance = self._accumulated_dist
+            feedback.current_mission_duration = curr_time
+            self._as.publish_feedback(feedback)
+            rospy.loginfo(feedback.accumulated_distance)
+            if self.goal_reach_control(goal_point):
                 self._goal_reached = True
-            self._r.sleep()
+            self._rate.sleep()
 
         if success:
-            if self._curr_time.secs < _goal_seconds:
-                self._result.goal_achieved = True
+            if curr_time.secs < goal_seconds:
+                result.goal_achieved = True
             else:
-                self._result.goal_achieved = False
-            self._result.total_time = self._curr_time
+                result.goal_achieved = False
+            result.total_time = curr_time
             rospy.loginfo('%s: Succeeded' % self._action_name)
-            self._as.set_succeeded(self._result)
+            self._as.set_succeeded(result)
 
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('action_server')
+        rospy.init_node('follow_line_action')
         server = RobotAction(rospy.get_name())
         rospy.spin()
     except rospy.ROSInterruptException:
